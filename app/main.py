e131fcf17e7b56ff59c8c30adb51825fc537e5d6
apjanco <apjanco@gmail.com>
2022-06-11 14:07:18 +0000

    is mvp

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import srsly
+from datetime import datetime
 from typing import Union
 from pathlib import Path
 from fastapi import FastAPI, UploadFile, Form, Request
@@ -8,6 +9,7 @@ from fastapi.templating import Jinja2Templates
 
 data_dir = Path.cwd() / 'app' / 'data' / 'json'
 assert data_dir.exists()
+current_counter = len(list(data_dir.iterdir()))
 
 images_dir = Path.cwd() / 'app' / 'data' / 'images'
 assert images_dir.exists()
@@ -22,15 +24,17 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/")
-async def form(nameOfBusiness: str = Form(''),latlong:str = Form(''), textOfAdvertisement: str= Form(''),notes:str =Form(''),message:str=Form(''), file: Union[UploadFile, None] = None):
+async def form(request:Request, myName:str = Form(''),nameOfBusiness: str = Form(''),latlong:str = Form(''), textOfAdvertisement: str= Form(''),notes:str =Form(''),message:str=Form(''), file: Union[UploadFile, None] = None):
     if file.filename != "":
         contents = await file.read()
-        (images_dir / file.filename).write_bytes(contents)
+        (images_dir / (datetime.now().strftime("%Y-%m-%d-%H:%M:%S") +"_" + file.filename)).write_bytes(contents)
     data = {"nameOfBusiness": nameOfBusiness, 
+            "myName":myName,
             "latlong":latlong, 
             "textOfAdvertisement": textOfAdvertisement, 
             "notes":notes,
             "message":message, 
             "filename":file.filename}
-    srsly.write_json((data_dir / 'is_data.json'), data)
-    return data
+    srsly.write_json((data_dir / f'{current_counter}_{datetime.now().strftime("%Y-%m-%d-%H:%M:%S")}.json'), data)
+    data["request"] = request
+    return templates.TemplateResponse("success.html", data)
